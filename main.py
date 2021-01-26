a76962b01dd1086064929d1050e22594514fc1be
jakubmatyszewski <jakubmatyszewski1@gmail.com>
2021-01-26 21:32:44 +0100

    Add html and connect to API

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from datetime import datetime
-from fastapi import Depends, FastAPI
 from typing import List, Union
-
+from fastapi import FastAPI, Depends, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 from sqlalchemy.orm import Session
 from db import models, schemas
@@ -10,6 +12,8 @@ from db.database import SessionLocal, engine
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 
 def get_db():
@@ -22,20 +26,39 @@ def get_db():
 
 # https://stackoverflow.com/questions/42552696/sqlalchemy-nearest-datetime
 @app.get("/news", response_model=List[schemas.SiteBase])
-def get_news(
-        date: Union[str, datetime] = datetime.now(),
-        db: Session = Depends(get_db),
-        site: str = 'tvn24',
-        limit: int = 1):
+def get_news(date: Union[str, datetime] = datetime.now(),
+             db: Session = Depends(get_db),
+             site: str = 'tvn24',
+             limit: int = 1):
     sites = {'tvn24': models.Tvn24, 'tvpinfo': models.Tvpinfo}
     if isinstance(date, str):
+        date_format = '%Y-%m-%dT%H:%M:%S'
         if len(date) <= 10:
             date += 'T12:00:00'
-        date = datetime.strptime(date[:19], '%Y-%m-%dT%H:%M:%S')
+        else:
+            date_format = date_format[:len(date) - 2]
+        date = datetime.strptime(date[:19], date_format)
     return db.query(sites[site]).filter(sites[site].time_stamp <= date).\
         order_by(sites[site].time_stamp.desc()).limit(limit).all()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request}
+    )
+
+
+@app.post("/")
+async def form_submit(request: Request,
+                      db: Session = Depends(get_db),
+                      date: str = Form(...),
+                      site: str = Form(...)):
+    results = get_news(date=date,
+                       db=db,
+                       site=site)
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request, "results": results}
+    )
--- test_main.py
+++ test_main.py
@@ -4,10 +4,10 @@ from main import app
 client = TestClient(app)
 
 
-def test_hello_world():
+def test_root_is_up():
     response = client.get('/')
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+    assert '<h1>News Sites API</h1>' in response.text
 
 
 def test_get_news_headline():
@@ -19,11 +19,18 @@ def test_get_news_headline():
 
 def test_get_news_headlines_closest_to_date():
     with TestClient(app) as client:
-        response = client.get('/news?site=tvn24&date=2021-01-23&limit=10')
+        response = client.get('/news?site=tvn24&date=2021-01-01&limit=10')
         assert response.status_code == 200
         assert len(response.json()) == 10
 
-        response = client.get('/news?site=tvn24&date=2021-01-23T12:30:00\
+        response = client.get('/news?site=tvn24&date=2021-01-01T12:30:00\
 &limit=10')
         assert response.status_code == 200
         assert len(response.json()) == 10
+
+
+def test_form_post():
+    headers = {'content-type': 'application/x-www-form-urlencoded'}
+    test_request_payload = {"date": "2021-01-01T12:00", "site": "tvn24"}
+    response = client.post('/', data=test_request_payload, headers=headers)
+    assert response.status_code == 200
